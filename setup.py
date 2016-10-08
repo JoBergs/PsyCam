6f14e7f49f72105bffa2efd94b65801128dba448
JoBergs <jo@knight-of-pi.org>
2016-10-08 17:41:59 +0200

    installation procedure: use pip locally

--- setup.py
+++ setup.py
@@ -4,7 +4,16 @@ from setuptools import setup
 
 from setuptools.command.install import install
 
-from install_tools import *
+from install_tools import install_apt_packages, activate_camera
+
+# NEWER:
+#   mkdir deepdream && cd deepdream
+#   git clone https://github.com/JoBergs/PsyCam
+#   cd PsyCam
+#   sudo pip install .
+#   sudo python install_tools.py caffe
+#   ect.
+
 
 # NEW: does this work???
 # sudo pip install psycam
@@ -25,6 +34,7 @@ from install_tools import *
 
 def do_before():
     print("\n\nInstalling DeepDream. No no he's not dead, he's, he's restin'!\n\n")    
+    # OFF FOR TESTING!
     install_apt_packages()
 
 def do_after():  
@@ -37,21 +47,28 @@ class CustomInstall(install):
         install.run(self)
         do_after()
 
-class InstallProtobuf:
-    def run(self):
-        install_protobuf()
+# class InstallCaffe:
+#     def run(self):
+#         install_caffe()
 
-class InstallPsyCam:
-    def run(self):
-        install_pyscam()
+# class InstallProtobuf:
+#     def run(self):
+#         install_protobuf()
+
+# class InstallPsyCam:
+#     def run(self):
+#         install_pyscam()
 
 setup(
     name='PsyCam',
-    version='1.4',
+    version='1.9',
     description='Install Google DeepDream on a Raspberry Pi with Raspbian Jessie',
     author='Johannes Bergs',
+    #scripts=['install_tools'],
     author_email='jo@knight-of-pi.org',
+    # OFF FOR TESTING
     install_requires=['pyzmq', 'jsonschema', 'pillow', 'numpy', 'scipy', 'ipython', 'jupyter', 'pyyaml'],
     url='https://github.com/JoBergs/PsyCam',
-    cmdclass={'install': CustomInstall, 'caffe': InstallCaffe, 'protobuf': InstallProtobuf, 'psycam': InstallPsyCam}
+    cmdclass={'install': CustomInstall}
+    #cmdclass={'install': CustomInstall, 'caffe': InstallCaffe, 'protobuf': InstallProtobuf, 'psycam': InstallPsyCam}
 )
