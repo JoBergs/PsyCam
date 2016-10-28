04bcb69805791f8f24eed851f7b2ba305119c0d2
JoBergs <jo@knight-of-pi.org>
2016-10-28 17:03:09 +0200

    updating installer for non-rpi computers: sudo necessary for ubuntu, hdf5 lib path changed

--- install_tools.py
+++ install_tools.py
@@ -3,15 +3,20 @@ import os, shutil, subprocess, sys
 HOME = os.path.expanduser("~")
 
 
-def execute_commands(path, commands):
+def execute_commands(path, commands, ubuntu):
     os.chdir(path)
 
     for command in commands:
+        if ubuntu:
+            command = 'sudo ' + command
         subprocess.call([command], shell=True)
 
 
 ########### camera ###########
-def activate_camera():
+def activate_camera(ubuntu=False):
+    if ubuntu:
+        print('Camera not activated for Ubuntu system!')
+        return
 
     with open('/boot/config.txt', "r") as f:
         config = f.read()
@@ -31,7 +36,7 @@ apt_packages = ["gfortran", "cython",
     "--no-install-recommends libboost-all-dev",
     "python-dev", "libgflags-dev", "libgoogle-glog-dev", "liblmdb-dev", "libatlas-base-dev", "python-skimage"]
 
-def install_packages():
+def install_packages(ubuntu=False):
     subprocess.call(['sudo apt-get update && sudo apt-get -y upgrade'], shell=True)
 
     # TODO: fuse strings and pass to execute_commands
@@ -45,13 +50,13 @@ def install_packages():
 caffe_commands = ['make all', 'make runtest', 'make pycaffe',
                               'make test', './scripts/download_model_binary.py models/bvlc_googlenet']
 
-caffe_replace = (('# CPU_ONLY := 1', 'CPU_ONLY := 1'),
-                         ('/usr/lib/python2.7/dist-packages/numpy/core/include',
-                          '/usr/local/lib/python2.7/dist-packages/numpy/core/include'),
-                         ('INCLUDE_DIRS := $(PYTHON_INCLUDE) /usr/local/include',
-                         'INCLUDE_DIRS := $(PYTHON_INCLUDE) /usr/local/include /usr/include/hdf5/serial/'),
-                         ('LIBRARY_DIRS := $(PYTHON_LIB) /usr/local/lib /usr/lib',
-                         'LIBRARY_DIRS := $(PYTHON_LIB) /usr/local/lib /usr/lib /usr/lib/arm-linux-gnueabihf/hdf5/serial/'))
+caffe_replace = [['# CPU_ONLY := 1', 'CPU_ONLY := 1'],
+                         ['/usr/lib/python2.7/dist-packages/numpy/core/include',
+                          '/usr/local/lib/python2.7/dist-packages/numpy/core/include'],
+                         ['INCLUDE_DIRS := $(PYTHON_INCLUDE) /usr/local/include',
+                         'INCLUDE_DIRS := $(PYTHON_INCLUDE) /usr/local/include /usr/include/hdf5/serial/'],
+                         ['LIBRARY_DIRS := $(PYTHON_LIB) /usr/local/lib /usr/lib',
+                         'LIBRARY_DIRS := $(PYTHON_LIB) /usr/local/lib /usr/lib /usr/lib/arm-linux-gnueabihf/hdf5/serial/']]
 
 #base_path = '/home/pi'
 dd_path = os.path.join(HOME, 'deepdream')
@@ -59,7 +64,7 @@ caffe_path = os.path.join(dd_path, 'caffe')
 bash_path = os.path.join(HOME, '.bashrc')
 
 
-def install_caffe():
+def install_caffe(ubuntu=False):
 
     # abort if caffe is already installed
     if os.path.isdir(caffe_path):
@@ -77,13 +82,17 @@ def install_caffe():
     with open('Makefile.config', "r") as f:
         makefile = f.read()
 
+    # hdf5 is at a different place when installing for ubuntu
+    if ubuntu:
+        caffe_replace[-1][-1] = "LIBRARY_DIRS := $(PYTHON_LIB) /usr/local/lib /usr/lib /usr/lib/x86_64-linux-gnu/hdf5/serial/"
+
     for item in caffe_replace:
         makefile = makefile.replace(item[0], item[1])
 
     with open('Makefile.config', "w") as f:
         f.write(makefile)
 
-    execute_commands('.', caffe_commands)
+    execute_commands('.', caffe_commands, ubuntu)
 
     with open(bash_path, "a") as f:
         f.write("export PYTHONPATH=" + HOME + "/deepdream/caffe/python:$PYTHONPATH")
@@ -98,7 +107,7 @@ python_protobuf = ['python setup.py build', 'python setup.py google_test', 'sudo
 protobuf_path = os.path.join(dd_path, 'protobuf-2.6.1')
 protobuf_python_path = os.path.join(protobuf_path, 'python')
 
-def install_protobuf():
+def install_protobuf(ubuntu=False):
     # abort if protobuf is already installed
     if os.path.isdir(protobuf_path):
         return
@@ -110,23 +119,28 @@ def install_protobuf():
 
     subprocess.call(['cd protobuf-2.6.1'], shell=True)
 
-    execute_commands('.', download_protobuf)
-    execute_commands(protobuf_path, make_protobuf)
-    execute_commands(protobuf_python_path, python_protobuf)
+    execute_commands('.', download_protobuf, ubuntu)
+    execute_commands(protobuf_path, make_protobuf, ubuntu)
+    execute_commands(protobuf_python_path, python_protobuf, ubuntu)
 ########### /protobuf ###########
 
 if __name__ == "__main__":
 
-    
-    if len(sys.argv) == 2:
+    # flag for installing on a host computer with ubuntu or 
+    # on the RPi with Raspbian
+    ubuntu = False
+
+    if len(sys.argv) >= 2:
+        if len(sys.argv) == 3 and sys.argv[2] in ['-u', '--ubuntu']:
+            ubuntu = True
         if sys.argv[1] == "packages":
-            install_packages()  
+            install_packages(ubuntu)  
         elif sys.argv[1] == "caffe":
-            install_caffe()        
+            install_caffe(ubuntu)        
         elif sys.argv[1] == "protobuf":
-            install_protobuf()        
+            install_protobuf(ubuntu)        
         elif sys.argv[1] == "camera":
-            activate_camera()
+            activate_camera(ubuntu)
 
     else:
         print("""What should be installed?\nPass 'packages', 'caffe', 'protobuf'
